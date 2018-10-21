7a2ec200f0d32c0c3732d77fcc5f57a1a8821847
kaiyuanliu <kaiyuanliu@healthlink.cn>
2018-10-21 18:00:26 +0800

    调整了页面样式，配置 vue.config.js 文件

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,34 @@
+const path = require('path')
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  outputDir: 'dist',
+  assetsDir: './',
   lintOnSave: false,
   filenameHashing: false,
   productionSourceMap: false,
   devServer: {
+    //host:'localhost',
+    //open:true,
     port: 5678,
-    proxy: {
-      '/api':{
-        target:'http://172.16.212.45:8079',
-        changeOrigin:true,
-        pathRewrite:{
-          '/api':''
-        }
-      }
-    },
-  }
+    // proxy: {
+    //   '/api':{
+    //     target:'http://172.16.212.45:8079',
+    //     changeOrigin:true,
+    //     pathRewrite:{
+    //       '/api':''
+    //     }
+    //   }
+    // },
+    proxy: process.env.VUE_APP_API_URL
+  },
+  configureWebpack: (config) => {    
+    if (process.env.NODE_ENV === 'production') {
+      //  为生产环境修改配置...            
+    } else {
+      //  为开发环境修改配置...      
+    }
+    config.resolve.alias['@com'] = path.resolve(__dirname, './src/components');//  添加别名    
+    config.resolve.extensions.push('.css');// 添加可省略的扩展名
+    console.log(config)
+  },
 };
